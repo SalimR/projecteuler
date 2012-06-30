9433d5f1c4c8262c257eb7595d6abcf1de7a64a8
Salim Rechenberg <SalimR@gmx.de>
2012-06-30 22:01:41 +0200

    initial commit

---
+++ src/main/java/projecteuler/App.java
@@ -0,0 +1,13 @@
+package projecteuler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
